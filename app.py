5fc386abfd24cb7c85bcf9494778833373a32c49
Shen <slee051@e.ntu.edu.sg>
2017-02-21 17:22:03 +0800

    updates

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ import spell
 from bottle import request, route, run, Response, template
 
 @route("/")
+@route("/home")
 def home():
-	init()
 	return template('index.tpl', request=request)
 
 @route("/check")
