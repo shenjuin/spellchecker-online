7391a12aa6416d60d2f13aaccc5afce7a1e2ab98
Shen <slee051@e.ntu.edu.sg>
2017-02-21 15:54:19 +0800

    misc updates

--- app.py
+++ app.py
@@ -6,8 +6,18 @@ import bottle
 import os
 import sys
 
-# routes contains the HTTP handlers for our server and must be imported.
-import routes
+from bottle import request, route, run, Response, template, view
+
+from datetime import datetime
+
+@route('/')
+@route('/home')
+@view('index')
+def home():
+    """Renders the home page."""
+    return dict(
+        year=datetime.now().year
+    )
 
 if '--debug' in sys.argv[1:] or 'SERVER_DEBUG' in os.environ:
     # Debug mode will enable more verbose output in the console window.
